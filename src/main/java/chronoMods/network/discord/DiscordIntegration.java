a909b8175421d17c369fd37acd174039095e510e
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-05-15 16:16:15 -0400

    start cleaning mess

--- src/main/java/chronoMods/network/discord/DiscordIntegration.java
+++ src/main/java/chronoMods/network/discord/DiscordIntegration.java
@@ -21,7 +21,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
